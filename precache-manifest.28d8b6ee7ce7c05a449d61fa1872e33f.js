6cfef01248a1740b5bb186d5efb73a63ae29b895
Deepika <bhuvanadeepika017@gmail.com>
2020-08-29 23:00:22 +0530

    Updates

---
+++ precache-manifest.28d8b6ee7ce7c05a449d61fa1872e33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2461044e2cf3775bc5096d4966a1428b",
+    "url": "/todoWithJsx/index.html"
+  },
+  {
+    "revision": "d78a3fbccf60a05f4372",
+    "url": "/todoWithJsx/static/css/main.fbbc1405.chunk.css"
+  },
+  {
+    "revision": "ffa0ce1f5bcb805acb5f",
+    "url": "/todoWithJsx/static/js/2.cbb27b25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoWithJsx/static/js/2.cbb27b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78a3fbccf60a05f4372",
+    "url": "/todoWithJsx/static/js/main.eb1f97ac.chunk.js"
+  },
+  {
+    "revision": "43eb7736190b5c6aa37a",
+    "url": "/todoWithJsx/static/js/runtime-main.539de731.js"
+  }
+]);
